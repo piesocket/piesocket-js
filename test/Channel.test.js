9b98ca0522104e8f0e022a6069dc93850db35b19
Anand Singh <singh.ak700@gmail.com>
2022-01-15 19:25:12 +0530

    Add channel tests

---
+++ test/Channel.test.js
@@ -0,0 +1,93 @@
+import '@babel/polyfill';
+import Channel from '../src/Channel';
+import PieSocket from '../src/PieSocket';
+import Logger from '../src/Logger';
+
+const assert = require('assert');
+
+const mockWebSocketClose = jest.fn();
+const mockWebSocketSend = jest.fn();
+jest.mock('../src/Socket.js', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      close: mockWebSocketClose,
+      send: mockWebSocketSend
+    };
+  });
+});
+
+const channelProperties = ['events', 'listeners','endpoint', 'identity', 'connection', 'shouldReconnect', 'logger'];
+const uninitializedChannelProperties = ['events', 'listeners'];
+
+describe('Channel', function () {
+
+  let piesocket;
+  let channel;
+
+  beforeAll(()=>{
+    piesocket = new PieSocket();
+    channel = new Channel(piesocket.getEndpoint("test"), piesocket.options);
+  });
+
+  it('#constructor() - Returns Channel Instance', function () {
+    expect(Object.keys(channel)).toEqual(channelProperties);
+
+    assert.deepEqual(channel.identity, { ...piesocket.options });
+    assert.deepEqual(channel.events, {});
+    assert.deepEqual(channel.listeners, {});
+    assert.equal(channel.shouldReconnect, false);
+    assert.deepEqual(channel.logger, new Logger(channel.identity));
+  });
+
+  it('#init() - Initializes Uninitialized Channel Instance', function () {
+    const uninitializedChannel = new Channel(null, null, false);
+
+    expect(Object.keys(uninitializedChannel)).toEqual(uninitializedChannelProperties);
+    const connectSpy = jest.spyOn(uninitializedChannel, 'connect');
+    uninitializedChannel.init(piesocket.getEndpoint("test"), piesocket.options);
+    
+    const propsAfterInit = Object.keys(uninitializedChannel);
+    propsAfterInit.splice(2,1); //remove connect property added by spy
+    expect(propsAfterInit).toEqual(channelProperties);
+
+    expect(connectSpy).toHaveBeenCalled()
+
+    assert.deepEqual(uninitializedChannel.identity, { ...piesocket.options });
+    assert.deepEqual(uninitializedChannel.events, {});
+    assert.deepEqual(uninitializedChannel.listeners, {});
+    assert.equal(uninitializedChannel.shouldReconnect, false);
+    assert.deepEqual(uninitializedChannel.logger, new Logger(channel.identity));
+  });
+
+  it('#on() - Registers event callbacks', function () {
+    const callback = jest.fn();
+    channel.on('open', callback);
+    expect(channel.events['open']).toEqual(callback);
+  })
+
+  it('#listen() - Registers websocket onmessage event listeners', function () {
+    const callback = jest.fn();
+    channel.listen('testevent', callback);
+    expect(channel.listeners['testevent']).toEqual(callback);
+  })
+
+  it('#send() - Sends message on websocket connection', function () {
+    channel.send('testdata');
+    expect(mockWebSocketSend).toHaveBeenCalledWith("testdata");
+  })
+
+  it('#publish() - Sends formatted event on websocket connection', function () {
+    channel.publish('testevent', {
+        "name": "John Doe"
+    }, { "foo": "bar" });
+    expect(mockWebSocketSend).toHaveBeenCalledWith(JSON.stringify({
+        event: "testevent",
+        data: {
+            "name": "John Doe"
+        },
+        meta: { "foo": "bar" }
+    }));
+  });
+
+
+});
--- test/PieSocket.test.js
+++ test/PieSocket.test.js
@@ -7,8 +7,13 @@ import Socket from '../src/Socket';
 const pieSocketProperties = ['options', 'connections', 'logger'];
 const channelProperties = ['events', 'listeners', 'endpoint', 'identity', 'connection', 'shouldReconnect', 'logger'];
 
+const mockWebSocketClose = jest.fn();
 jest.mock('../src/Socket.js', () => {
-  return jest.fn();
+  return jest.fn().mockImplementation(() => {
+    return {
+      close: mockWebSocketClose
+    };
+  });;
 });
 
 var assert = require('assert');
@@ -17,17 +22,16 @@ describe('PieSocket', function () {
 
   describe('#constructor()', function () {
 
-    it('Test default options', function () {
-      const piesocket = new PieSocket({});
+    it('PieSocket constructor returns ok', function () {
+      const piesocket = new PieSocket();
       assert.deepEqual(piesocket.options, DefaultOptions);
       assert.deepEqual(piesocket.connections, {});
       assert.deepEqual(piesocket.logger, new Logger(piesocket.options));
 
       expect(Object.keys(piesocket)).toEqual(pieSocketProperties);
-
     });
 
-    it('Test option override', function () {
+    it('Option overrides work', function () {
       const piesocket = new PieSocket({
         clusterId: 'nyc1',
         apiKey: 'xxx'
@@ -37,15 +41,19 @@ describe('PieSocket', function () {
 
   });
 
-  describe('#subscribe()', function () {
+  describe('Channel functions', function () {
 
-    it('Test channel subscription', done => {
-      const piesocket = new PieSocket({
+    let piesocket;
+    let channel;
+
+    beforeAll(done => {
+      piesocket = new PieSocket({
         clusterId: 'nyc1',
         apiKey: 'xxx'
       });
 
-      function subscribeCallback(channel) {
+      function subscribeCallback(_channel) {
+        channel = _channel;
         expect(Socket).toHaveBeenCalledWith('wss://nyc1.piesocket.com/v3/test-channel?api_key=xxx&notify_self=0&source=jssdk&v=1.4.0&presence=0');
         assert.deepEqual(channel.identity, { ...piesocket.options, channelId: 'test-channel' });
         assert.deepEqual(channel.events, {});
@@ -56,8 +64,127 @@ describe('PieSocket', function () {
         done();
       }
 
-
       piesocket.subscribe('test-channel').then(subscribeCallback);
+    });
+
+    it('#subscribe - Creates channel connection', () => {
+      expect(Object.keys(channel)).toEqual(channelProperties);
+    });
+
+    it('#subscribe - Creates another channel subscription', done => {
+      piesocket.subscribe('test-channel-2').then(()=>{
+        expect(Object.keys(piesocket.connections).length).toEqual(2);
+        done();    
+      });
+    });
+
+    it('#subscribe - Prevents duplicate channel connection', done => {
+      piesocket.subscribe('test-channel').then(()=>{
+        expect(Object.keys(piesocket.connections).length).toEqual(2);
+        done();    
+      });
+    });
+
+    it('#unsubscribe - Removes channel subscription', () => {
+      const result = piesocket.unsubscribe("test-channel");
+      expect(result).toEqual(true);
+      expect(Object.keys(piesocket.connections).length).toEqual(1);
+    });
+
+    it('#unsubscribe - Removes non-existing channel subscription', () => {
+      const result = piesocket.unsubscribe("test-channel-99");
+      expect(result).toEqual(false);
+      expect(Object.keys(piesocket.connections).length).toEqual(1);
+    });
+
+    it('#getConnections - Returns all connections', () => {
+      const result = piesocket.getConnections();
+      assert.deepEqual(result, piesocket.connections);
+    });
+
+    it('#isGuarded - Returns true for private- prefixed channels', () => {
+      const result = piesocket.isGuarded('private-channel');
+      expect(result).toEqual(true);
+    });
+
+    it('#isGuarded - Returns false for channels not prefixed with private-', () => {
+      const result = piesocket.isGuarded('dummy-channel');
+      expect(result).toEqual(false);
+    });
+
+    it('#isGuarded - Returns true for any channel when forceAuth is set', () => {
+      piesocket.options.forceAuth = true;      
+      const result = piesocket.isGuarded('dummy-channel');
+      expect(result).toEqual(true);
+    });
+
+    it('#getEndpoint - Returns websocket endpoint', done => {
+      piesocket.options.forceAuth = false;      
+      piesocket.getEndpoint('test-channel').then((result) => {
+        expect(result).toEqual('wss://nyc1.piesocket.com/v3/test-channel?api_key=xxx&notify_self=0&source=jssdk&v=1.4.0&presence=0');
+        done();
+      });
+    });
+
+    it('#getEndpoint - Returns websocket endpoint with JWT', done => {
+      piesocket.options.jwt = 'testcode';      
+      piesocket.getEndpoint('test-channel').then((result) => {
+        expect(result).toEqual('wss://nyc1.piesocket.com/v3/test-channel?api_key=xxx&notify_self=0&source=jssdk&v=1.4.0&presence=0&jwt=testcode');
+        done();
+      });
+    });
+
+    it('#getEndpoint - Returns websocket endpoint with auto-generated JWT when forceAuth is set', done => {
+      piesocket.options.jwt = null;      
+      piesocket.options.forceAuth = true;
+      piesocket.getAuthToken = jest.fn().mockImplementation(()=>{
+        return  Promise.resolve({
+          auth: 'test-token'
+        });
+      });
+
+      piesocket.getEndpoint('test-channel').then((result) => {
+        expect(result).toEqual('wss://nyc1.piesocket.com/v3/test-channel?api_key=xxx&notify_self=0&source=jssdk&v=1.4.0&presence=0&jwt=test-token');
+        done();
+      });
+    });
+
+    it('#getEndpoint - Returns websocket endpoint without auto-generated JWT when auth endpoint call returns invalid data', done => {
+      piesocket.options.jwt = null;      
+      piesocket.options.forceAuth = true;
+      piesocket.getAuthToken = jest.fn().mockImplementation(()=>{
+        return  Promise.resolve("blabla");
+      });
+
+      piesocket.getEndpoint('test-channel').then((result) => {
+        expect(result).toEqual('wss://nyc1.piesocket.com/v3/test-channel?api_key=xxx&notify_self=0&source=jssdk&v=1.4.0&presence=0');
+        done();
+      });
+    });
+
+    it('#getEndpoint - Returns websocket endpoint with auto-generated JWT for private channels', done => {
+      piesocket.options.jwt = null;      
+      piesocket.options.forceAuth = false;
+      piesocket.getAuthToken = jest.fn().mockImplementation(()=>{
+        return  Promise.resolve({
+          auth: 'test-token'
+        });
+      });
+
+      piesocket.getEndpoint('private-channel').then((result) => {
+        expect(result).toEqual('wss://nyc1.piesocket.com/v3/private-channel?api_key=xxx&notify_self=0&source=jssdk&v=1.4.0&presence=0&jwt=test-token');
+        done();
+      });
+    });
+
+
+    it('#getEndpoint - Returns websocket endpoint with user identity', done => {
+      piesocket.options.forceAuth = false;      
+      piesocket.options.userId = 1;      
+      piesocket.getEndpoint('test-channel').then((result) => {
+        expect(result).toEqual('wss://nyc1.piesocket.com/v3/test-channel?api_key=xxx&notify_self=0&source=jssdk&v=1.4.0&presence=0&user=1');
+        done();
+      });
     });
 
   });
