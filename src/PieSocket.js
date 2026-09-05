1ceec7ed4d4c22edd18b799b1e8e4e8069aa75f8
sudoanand <sudoanand@gmail.com>
2026-09-05 09:52:59 +0530

    v7.0.0

--- src/PieSocket.js
+++ src/PieSocket.js
@@ -7,7 +7,7 @@ import InvalidAuthException from './InvalidAuthException.js';
 import defaultOptions from './misc/DefaultOptions.js';
 import {v4 as uuidv4} from 'uuid';
 
-const SDK_VERSION = '6.1.0';
+const SDK_VERSION = '7.0.0';
 
 export default class PieSocket {
   constructor(options) {
